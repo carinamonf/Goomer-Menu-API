6f64255d2174cbd57492185548020854bead17ab
carinamonf <159257207+carinamonf@users.noreply.github.com>
2025-10-28 13:18:08 -0300

    feat: implement controller and routes for product crud

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express = require("express");
+import { produtoRoutes } from "./modules/produtos/produto.routes";
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3333;
+
+app.use('/api/produtos', produtoRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Goomer Menu API iniciado!' });
